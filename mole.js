6fea69c25454fdf89ae9ec7653a53fc79e263334
Zachary <duvallzt@gmail.com>
2020-12-03 19:02:08 -0500

    Complete everything up to winning the game

--- mole.js
+++ mole.js
@@ -1,38 +1,38 @@
 window.addEventListener("DOMContentLoaded", () => {
-  //   setInterval(() => {
-  //     const moleHeads = document.querySelectorAll('.wgs__mole-head');
-  //     for (let moleHead of moleHeads) {
-  //       moleHead.classList.toggle('wgs__mole-head--hidden');
-  //     }
-  //   }, 1000);
+    //   setInterval(() => {
+    //     const moleHeads = document.querySelectorAll('.wgs__mole-head');
+    //     for (let moleHead of moleHeads) {
+    //       moleHead.classList.toggle('wgs__mole-head--hidden');
+    //     }
+    //   }, 1000);
 
-  setTimeout(() => {
-    popUpRandomMole();
-  }, 1000);
+    setTimeout(() => {
+        popUpRandomMole();
+    }, 1000);
 
-  let moleHeads = Array.from(document.querySelectorAll(".wgs__mole-head"));
-moleHeads.forEach(moleHead => moleHead.addEventListener("click", (event) => {
-    event.target.classList.add("wgs__mole-head--hidden");
-    
-}))
+    let moleHeads = Array.from(document.querySelectorAll(".wgs__mole-head"));
+    moleHeads.forEach(moleHead => moleHead.addEventListener("click", (event) => {
+        event.target.classList.add("wgs__mole-head--hidden");
+        event.target.classList.add("wgs__mole-head--whacked");
+    }))
 
 });
 
 let hideMole = function (hiddenMole) {
-  hiddenMole.classList.add("wgs__mole-head--hidden");
+    hiddenMole.classList.add("wgs__mole-head--hidden");
 
-  setTimeout(() => {
-    popUpRandomMole();
-  }, 1000);
+    setTimeout(() => {
+        popUpRandomMole();
+    }, 1000);
 };
 
 let popUpRandomMole = function () {
-  let moleHeads = Array.from(document.querySelectorAll(".wgs__mole-head"));
-  let iMole = Math.floor(Math.random() * 8);
-  let hiddenMole = moleHeads[iMole]
-  hiddenMole.classList.remove("wgs__mole-head--hidden");
+    let moleHeads = Array.from(document.querySelectorAll(".wgs__mole-head:not(.wgs__mole-head--whacked)"));
+    let iMole = Math.floor(Math.random() * moleHeads.length);
+    let hiddenMole = moleHeads[iMole]
+    hiddenMole.classList.remove("wgs__mole-head--hidden");
 
-  setTimeout(() => {
-    hideMole(hiddenMole);
-  }, 3000);
+    setTimeout(() => {
+        hideMole(hiddenMole);
+    }, 2500);
 };
